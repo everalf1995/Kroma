a721d8c7966b88dd064642c570b61604be7ecad8
Ever Barillas <everalf1995@gmail.com>
2018-05-21 02:15:08 -0400

    Working on the logo

--- app/src/main/java/kronikle/project/Landing/LandingActivity.java
+++ app/src/main/java/kronikle/project/Landing/LandingActivity.java
@@ -1,9 +1,11 @@
 package kronikle.project.Landing;
 
+import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import kronikle.project.R;
 import kronikle.project.ViewPagerAdapter;
@@ -12,6 +14,8 @@ public class LandingActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class LandingActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tab_layout_LA);
         viewPager = findViewById(R.id.view_pager_LA);
+        textView = findViewById(R.id.kronikle_LA);
 
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.AddFragment(new SignInFragment(), getString(R.string.sign_in));
