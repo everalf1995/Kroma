55785e33337feef7b916bc27f6e7e7b6d9ce32d5
Ever Barillas <everalf1995@gmail.com>
2018-05-24 23:28:40 -0400

    Implemented error messages for Sign In and Sign Up Fragments

--- app/src/main/java/kronikle/project/Landing/SignInFragment.java
+++ app/src/main/java/kronikle/project/Landing/SignInFragment.java
@@ -1,5 +1,7 @@
 package kronikle.project.Landing;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
@@ -9,16 +11,17 @@ import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
-
 import java.util.Objects;
 
+import kronikle.project.MainActivity;
 import kronikle.project.R;
 
 public class SignInFragment extends Fragment {
@@ -30,6 +33,10 @@ public class SignInFragment extends Fragment {
     TextInputLayout textInputLayoutPassword;
     TextInputEditText editTextPassword;
     Button buttonSignIn;
+    Button forgotPassword;
+    Button continueGuest;
+    Button facebook;
+    Button google;
 
     public SignInFragment() {
 
@@ -41,9 +48,7 @@ public class SignInFragment extends Fragment {
         view = inflater.inflate(R.layout.sign_in_fragment, container, false);
 
         initializer();
-
-        editTextEmail.addTextChangedListener(new textWatcher(editTextEmail));
-        editTextPassword.addTextChangedListener(new textWatcher(editTextPassword));
+        textChangeListener();
 
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,15 +60,22 @@ public class SignInFragment extends Fragment {
         return view;
     }
 
-    public void initializer() {
-
+    private void initializer() {
         textInputLayoutEmail = view.findViewById(R.id.text_input_layout_email_SIF);
         editTextEmail = view.findViewById(R.id.edit_text_email_SIF);
         textInputLayoutPassword = view.findViewById(R.id.text_input_layout_password_SIF);
         editTextPassword = view.findViewById(R.id.edit_text_password_SIF);
         buttonSignIn = view.findViewById(R.id.button_sign_in_SIF);
+        forgotPassword = view.findViewById(R.id.button_forgot_password_SIF);
+        continueGuest = view.findViewById(R.id.button_continue_guest_SIF);
+        facebook = view.findViewById(R.id.button_facebook_SIF);
+        google = view.findViewById(R.id.button_google_SIF);
     }
 
+    public void textChangeListener() {
+        editTextEmail.addTextChangedListener(new textWatcher(editTextEmail));
+        editTextPassword.addTextChangedListener(new textWatcher(editTextPassword));
+    }
 
     private class textWatcher implements TextWatcher {
 
@@ -92,53 +104,58 @@ public class SignInFragment extends Fragment {
         }
     }
 
-    public void validateInformation() {
-
-        if (!validateEmail()) {
-            return;
-        }
-
-        if (!validatePassword()) {
-            return;
-        }
-
-        Toast.makeText(getContext(), "Thank You!", Toast.LENGTH_SHORT).show();
-    }
-
-
     private boolean validateEmail() {
         String email = editTextEmail.getText().toString().trim();
 
-        if (email.isEmpty() || !isValidEmail(email)) {
-            textInputLayoutEmail.setError(getString(R.string.error_email));
-            requestFocus(editTextEmail);
+        if (email.isEmpty() || !(!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            textInputLayoutEmail.setError(getString(R.string.error_email_sif));
             return false;
-        } else {
-            textInputLayoutEmail.setErrorEnabled(false);
         }
 
-        return true;
+        else {
+            textInputLayoutEmail.setErrorEnabled(false);
+            return true;
+        }
     }
 
     private boolean validatePassword() {
-        if (editTextPassword.getText().toString().trim().isEmpty()) {
-            textInputLayoutPassword.setError(getString(R.string.error_password));
-            requestFocus(editTextPassword);
+        if (editTextPassword.getText().toString().trim().isEmpty() || editTextPassword.getText().toString().trim().length() < 8) {
+            textInputLayoutPassword.setError(getString(R.string.error_password_sif));
             return false;
-        } else {
+        }
+
+        else {
             textInputLayoutPassword.setErrorEnabled(false);
+            return true;
         }
-
-        return true;
     }
 
-    private static boolean isValidEmail(String email) {
-        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    public void validateInformation() {
+
+        if (!validateEmail()) {
+            focusEditText(editTextEmail);
+        }
+
+        else if (!validatePassword()) {
+            focusEditText(editTextPassword);
+        }
+
+        else {
+            //Code to check if the given email is in the database
+
+            Toast.makeText(getContext(), "Welcome to KORNIKLE!", Toast.LENGTH_SHORT).show();
+
+            Intent MainActivityIntent = new Intent(getActivity(), MainActivity.class);
+            startActivity(MainActivityIntent);
+            Objects.requireNonNull(getActivity()).overridePendingTransition(R.anim.enter_in_up, R.anim.exit_out_up);
+            getActivity().finish();
+        }
     }
 
-    private void requestFocus(View view) {
-        if (view.requestFocus()) {
-            Objects.requireNonNull(getActivity()).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-        }
+    private void focusEditText(View view) {
+        view.requestFocus();
+        InputMethodManager inputMethod = (InputMethodManager) Objects.requireNonNull(getActivity()).getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethod != null;
+        Objects.requireNonNull(inputMethod).showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
     }
 }
--- app/src/main/java/kronikle/project/Landing/SignUpFragment.java
+++ app/src/main/java/kronikle/project/Landing/SignUpFragment.java
@@ -1,18 +1,43 @@
 package kronikle.project.Landing;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.Toast;
 
+import java.util.Objects;
+
+import kronikle.project.MainActivity;
 import kronikle.project.R;
 
 public class SignUpFragment extends Fragment {
 
     View view;
+    TextInputLayout textInputLayoutFirstName;
+    TextInputEditText editTextFirstName;
+    TextInputLayout textInputLayoutLastName;
+    TextInputEditText editTextLastName;
+    TextInputLayout textInputLayoutEmail;
+    TextInputEditText editTextEmail;
+    TextInputLayout textInputLayoutPassword;
+    TextInputEditText editTextPassword;
+    TextInputLayout textInputLayoutConfirmPassword;
+    TextInputEditText editTextConfirmPassword;
+    Button buttonSignUp;
 
     public SignUpFragment() {
 
@@ -22,6 +47,186 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.sign_up_fragment, container, false);
+
+        initializer();
+        textChangeListener();
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateInformation();
+            }
+        });
+
         return view;
+    }
+
+    private void initializer() {
+        textInputLayoutFirstName = view.findViewById(R.id.text_input_layout_first_name_SUF);
+        editTextFirstName = view.findViewById(R.id.edit_text_first_name_SUF);
+        textInputLayoutLastName = view.findViewById(R.id.text_input_layout_last_name_SUF);
+        editTextLastName = view.findViewById(R.id.edit_text_last_name_SUF);
+        textInputLayoutEmail = view.findViewById(R.id.text_input_layout_email_SUF);
+        editTextEmail = view.findViewById(R.id.edit_text_email_SUF);
+        textInputLayoutPassword = view.findViewById(R.id.text_input_layout_password_SUF);
+        editTextPassword = view.findViewById(R.id.edit_text_password_SUF);
+        textInputLayoutConfirmPassword = view.findViewById(R.id.text_input_layout_confirm_password_SUF);
+        editTextConfirmPassword = view.findViewById(R.id.edit_text_confirm_password_SUF);
+        buttonSignUp = view.findViewById(R.id.button_sign_up_SUF);
+    }
+
+    public void textChangeListener() {
+        editTextFirstName.addTextChangedListener(new SignUpFragment.textWatcher(editTextFirstName));
+        editTextLastName.addTextChangedListener(new SignUpFragment.textWatcher(editTextLastName));
+        editTextEmail.addTextChangedListener(new SignUpFragment.textWatcher(editTextEmail));
+        editTextPassword.addTextChangedListener(new SignUpFragment.textWatcher(editTextPassword));
+        editTextConfirmPassword.addTextChangedListener(new SignUpFragment.textWatcher(editTextConfirmPassword));
+    }
+
+    private class textWatcher implements TextWatcher {
+
+        private View view;
+
+        private textWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.edit_text_first_name_SUF:
+                    validateFirstName();
+                    break;
+                case R.id.edit_text_last_name_SUF:
+                    validateLastName();
+                    break;
+                case R.id.edit_text_email_SUF:
+                    validateEmail();
+                    break;
+                case R.id.edit_text_password_SUF:
+                    validatePassword();
+                    break;
+                case R.id.edit_text_confirm_password_SUF:
+                    validateConfirmPassword();
+                    break;
+            }
+        }
+    }
+
+    private boolean validateFirstName() {
+        String firstName = editTextFirstName.getText().toString().trim();
+
+        if (firstName.isEmpty() || !firstName.matches("^[ A-Za-z]+$")) {
+            textInputLayoutFirstName.setError(getString(R.string.error_first_name_suf));
+            return false;
+        }
+
+        else {
+            textInputLayoutFirstName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateLastName() {
+        String lastName = editTextLastName.getText().toString().trim();
+
+        if (lastName.isEmpty() || !lastName.matches("[a-zA-Z]+")) {
+            textInputLayoutLastName.setError(getString(R.string.error_last_name_suf));
+            return false;
+        }
+
+        else {
+            textInputLayoutLastName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        String email = editTextEmail.getText().toString().trim();
+
+        if (email.isEmpty() || !(!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            textInputLayoutEmail.setError(getString(R.string.error_email_suf));
+            return false;
+        }
+
+        else {
+            textInputLayoutEmail.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String password = editTextPassword.getText().toString().trim();
+
+        if (password.isEmpty() || password.length() < 8) {
+            textInputLayoutPassword.setError(getString(R.string.error_password_suf));
+            return false;
+        }
+
+        else {
+            textInputLayoutPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateConfirmPassword() {
+        String password = editTextPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        if (!confirmPassword.equals(password)) {
+            textInputLayoutConfirmPassword.setError(getString(R.string.error_confirm_password_suf));
+            return false;
+        }
+
+        else {
+            textInputLayoutConfirmPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void validateInformation() {
+
+        if (!validateFirstName()) {
+            focusEditText(editTextFirstName);
+        }
+
+        else if (!validateLastName()) {
+            focusEditText(editTextLastName);
+        }
+
+        else if (!validateEmail()) {
+            focusEditText(editTextEmail);
+        }
+
+        else if (!validatePassword()) {
+            focusEditText(editTextPassword);
+        }
+
+        else if (!validateConfirmPassword()) {
+            focusEditText(editTextConfirmPassword);
+        }
+
+        else {
+            //Code to check if the given email is in the database
+
+            Toast.makeText(getContext(), "Welcome to KORNIKLE!", Toast.LENGTH_SHORT).show();
+
+            Intent MainActivityIntent = new Intent(getActivity(), MainActivity.class);
+            startActivity(MainActivityIntent);
+            Objects.requireNonNull(getActivity()).overridePendingTransition(R.anim.enter_in_up, R.anim.exit_out_up);
+            getActivity().finish();
+        }
+    }
+
+    private void focusEditText(View view) {
+        view.requestFocus();
+        InputMethodManager inputMethod = (InputMethodManager) Objects.requireNonNull(getActivity()).getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethod != null;
+        Objects.requireNonNull(inputMethod).showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
     }
 }
