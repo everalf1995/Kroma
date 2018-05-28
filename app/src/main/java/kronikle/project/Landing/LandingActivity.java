191192fec5627c4af368bc6479281a441bed2422
Ever Barillas <everalf1995@gmail.com>
2018-05-27 21:42:01 -0400

    -EditText loses focus when user clicks outside -Keyboard closes when switching fragment

--- app/src/main/java/kronikle/project/Landing/LandingActivity.java
+++ app/src/main/java/kronikle/project/Landing/LandingActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -22,7 +23,9 @@ public class LandingActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private TabLayout tabLayout;
     private ViewPager viewPager;
-    private TextView textView;
+    private TextView textViewKronikle;
+    private TextView textViewYourPersonal;
+    private TextView textViewFeatures;
 
 
     @Override
@@ -33,13 +36,16 @@ public class LandingActivity extends AppCompatActivity {
         initializer();
         tabLayoutInitializer();
         layoutFocus();
+        TextViewFeaturesStrings();
     }
 
     private void  initializer() {
         linearLayout = findViewById(R.id.linear_layout_LA);
         tabLayout = findViewById(R.id.tab_layout_LA);
         viewPager = findViewById(R.id.view_pager_LA);
-        textView = findViewById(R.id.kronikle_LA);
+        textViewKronikle = findViewById(R.id.text_view_kronikle_LA);
+        textViewYourPersonal = findViewById(R.id.text_view_your_personal_LA);
+        textViewFeatures = findViewById(R.id.text_view_features_LA);
     }
 
     public void tabLayoutInitializer() {
@@ -60,6 +66,20 @@ public class LandingActivity extends AppCompatActivity {
                 assert inputMethod != null;
                 inputMethod.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(), 0);
                 return true;
+            }
+        });
+    }
+
+    private void TextViewFeaturesStrings() {
+        final int[] array = {R.string.string_1, R.string.string_2, R.string.string_3, R.string.string_4, R.string.string_5, R.string.string_6};
+        textViewFeatures.post(new Runnable() {
+            int i = 0;
+            @Override
+            public void run() {
+                textViewFeatures.setText(array[i]);
+                i++;
+                if (i ==5) { i = 0; }
+                textViewFeatures.postDelayed(this, 2000);
             }
         });
     }
